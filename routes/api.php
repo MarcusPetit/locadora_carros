fbbe6e93573010eab30019f4d3fa370c89998a42
Marcus <marcuspetit@hotmail.com>
2024-10-29 04:49:48 -0300

    implementando JWP

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,15 @@ Route::apiResource('carro', CarroController::class);
 Route::apiResource('locacao', LocacaoController::class);
 Route::apiResource('marca', MarcaController::class);
 Route::apiResource('modelo', ModeloController::class);
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
